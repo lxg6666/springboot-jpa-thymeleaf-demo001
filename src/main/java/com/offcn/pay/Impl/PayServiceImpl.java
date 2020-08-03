cbdfd688f4a9e7394557db0f4e2d7de41b35e6d5
myjava001 <L15300921709@126.com>
2020-08-03 23:19:37 +0800

    校长提交修改

--- src/main/java/com/offcn/pay/Impl/PayServiceImpl.java
+++ src/main/java/com/offcn/pay/Impl/PayServiceImpl.java
@@ -7,12 +7,18 @@ import java.util.Map;
 public class PayServiceImpl implements PayService {
     @Override
     public void createNative(String out_trade_no, String price) {
-        System.out.println("预下单方法");
+        //校长修改
+        for (int i=0;i<100;i++) {
+            System.out.println("预下单方法");
+        }
+
     }
 
     @Override
     public Map queryStatus(String out_trade_no) {
-        System.out.println("查询交易状态代码");
+        if (out_trade_no!=null){
+            System.out.println("查询交易状态代码");
+        }
         return null;
     }
 }
